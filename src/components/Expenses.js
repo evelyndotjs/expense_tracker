ef9124412d8fbbb73ec3f5cb88cef6de353e8608
Evelyn Huang <evelyn@Evelyns-MacBook-Pro.local>
2022-01-04 15:07:09 +0100

    Create new expenses component

---
+++ src/components/Expenses.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.title}
+        amount={props.amount}
+        date={props.date}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
